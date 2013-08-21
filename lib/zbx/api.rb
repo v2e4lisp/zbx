8e3f009bd484758936cbfbb3b716153aed80bfde
wenjun.yan <wenjun.yan@gree.co.jp>
2013-08-21 16:54:05 +0900

    setter

--- lib/zbx/api.rb
+++ lib/zbx/api.rb
@@ -12,21 +12,28 @@ module ZBX
     #
     # ZBX::API.new(user, pass, api_url).host.get(hostids: 1)
 
-    def initialize user, pass, url, &b
+    def initialize user=nil, pass=nil, url=nil, &b
       @auth = nil
       @user = user
       @pass = pass
-      @uri = URI.parse(url)
-
-      # Our http client. Borrowed from zabbixapi(https://github.com/vadv/zabbixapi)
-      @http ||= Net::HTTP.new @uri.host, @uri.port
-      if @uri.port == 443
-        @http.use_ssl = true
-        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-
+      @api_url = url
       instance_eval(&b) if block_given?
     end
+
+    def username! username
+      @user, @auth = username, nil
+    end
+    alias_method :username=, :username!
+
+    def password! password
+      @pass, @auth = password, nil
+    end
+    alias_method :password=, :password!
+
+    def api_url! api_url
+      @api_url, @auth = api_url, nil
+    end
+    alias_method :api_url=, :api_url!
 
     def request method, params={}
       # in any api request except `user.login`
@@ -41,7 +48,8 @@ module ZBX
       _request(method: method,
                params: params,
                auth: auth!,
-               id: _id, jsonrpc: '2.0')
+               id: _id,
+               jsonrpc: '2.0')
     end
 
     def auth!
@@ -60,12 +68,26 @@ module ZBX
 
     private
 
+    def _http
+      # Our http client. Borrowed from zabbixapi(https://github.com/vadv/zabbixapi)
+      @http ||= Net::HTTP.new _uri.host, _uri.port
+      if _uri.port == 443
+        @http.use_ssl = true
+        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+      @http
+    end
+
+    def _uri
+      @uri ||= URI.parse(@api_url)
+    end
+
     def _request options={}
       # send post request
-      req = Net::HTTP::Post.new @uri.request_uri
+      req = Net::HTTP::Post.new _uri.request_uri
       req.add_field('Content-Type', 'application/json-rpc')
       req.body = options.to_json
-      JSON.parse(@http.request(req).body)['result']
+      JSON.parse(_http.request(req).body)['result']
     rescue
       nil
     end
--- lib/zbx/version.rb
+++ lib/zbx/version.rb
@@ -1,3 +1,3 @@
 module ZBX
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
