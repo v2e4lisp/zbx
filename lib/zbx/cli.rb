a7ea183be441d34da9d10aef3bd2a8e15c516789
wenjun.yan <wenjun.yan@gree.co.jp>
2013-08-26 11:11:32 +0900

    helper cli

--- lib/zbx/cli.rb
+++ lib/zbx/cli.rb
@@ -3,6 +3,19 @@ require 'thor'
 
 module ZBX
   module CLI
+    module Helper
+      extend self
+      def browse link
+        case RbConfig::CONFIG['host_os']
+        when /mswin|mingw|cygwin/
+          system "start #{link}"
+        when /darwin/
+          system "open #{link}"
+        when /linux|bsd/
+          system "xdg-open #{link}"
+        end
+      end
+    end
 
     class CLI < Thor
       map "-s" => :send
@@ -72,4 +85,3 @@ module ZBX
 
   end
 end
-
