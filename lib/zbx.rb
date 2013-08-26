efa63ac6cdf1697ef1ebda7b3db4feabc687ffa7
wenjun.yan <wenjun.yan@gree.co.jp>
2013-08-26 12:27:04 +0900

    bug

--- lib/zbx.rb
+++ lib/zbx.rb
@@ -7,7 +7,7 @@ require 'json'
 require 'net/http'
 
 module ZBX
-  def self.client user, password, api_url, &block
+  def self.client user=nil, password=nil, api_url=nil, &block
     API.new user, password, api_url, &block
   end
 end
