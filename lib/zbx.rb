860525fc87d614a1a15f777af3fecdff8c0d165e
wenjun.yan <wenjun.yan@gree.co.jp>
2013-08-26 20:21:42 +0900

    config

--- lib/zbx.rb
+++ lib/zbx.rb
@@ -7,7 +7,34 @@ require 'json'
 require 'net/http'
 
 module ZBX
-  def self.client user=nil, password=nil, api_url=nil, &block
-    API.new user, password, api_url, &block
+  class << self
+    # zbx module API, user should call this method to initialize a
+    # zabbxi-api client.
+    def client user=nil, password=nil, api_url=nil, &block
+      API.new(user || configuration.user,
+              password || configuration.password,
+              api_url || configuration.api_url,
+              &block)
+    end
+
+    # configuration
+    def config
+      yield(configuration)
+      configuration
+    end
+
+    def reset_configuration!
+      @configuration = nil
+    end
+
+    private
+
+    def configuration
+      @configuration ||= Struct.new(:user, :password, :api_url) {
+        def [] option
+          __send__ option
+        end
+      }.new
+    end
   end
 end
